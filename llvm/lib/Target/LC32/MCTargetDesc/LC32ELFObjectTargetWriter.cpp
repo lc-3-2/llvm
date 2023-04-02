686df35226763e22648908e1236fc471b79aed75
Ammar Ratnani <ammrat13@gmail.com>
2023-04-01 22:17:31 -0400

    Add fixup structures

---
+++ llvm/lib/Target/LC32/MCTargetDesc/LC32ELFObjectTargetWriter.cpp
@@ -0,0 +1,34 @@
+//===-- LC32ELFObjectWriter.h - LC-3.2 ELF Writer -------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "LC32ELFObjectTargetWriter.h"
+#include "LC32FixupKinds.h"
+using namespace llvm;
+using namespace llvm::LC32;
+#define DEBUG_TYPE "LC32ELFObjectTargetWriter"
+
+LC32ELFObjectTargetWriter::LC32ELFObjectTargetWriter(uint8_t OSABI)
+    : MCELFObjectTargetWriter(false, OSABI, ELF::EM_LC_3_2, true) {}
+
+unsigned LC32ELFObjectTargetWriter::getRelocType(MCContext &Ctx,
+                                                 const MCValue &Target,
+                                                 const MCFixup &Fixup,
+                                                 bool IsPCRel) const {
+  // For each fixup, emit the relocation it corresponds to
+  switch (Fixup.getTargetKind()) {
+    case FIXUP_32:
+      assert(!IsPCRel && "Bad fixup kind for PC-relative");
+      return ELF::R_LC_3_2_32;
+  }
+  // If there's no corresponding relocation, die
+  llvm_unreachable("No relocation for fixup");
+}
+
+std::unique_ptr<MCObjectTargetWriter> createLC32ELFObjectWriter(uint8_t OSABI) {
+  return std::make_unique<LC32ELFObjectTargetWriter>(OSABI);
+}
---
+++ llvm/lib/Target/LC32/MCTargetDesc/LC32FixupKinds.h
@@ -0,0 +1,38 @@
+//===-- LC32FixupKinds.h - LC-3.2 Assembly Fixup Entries --------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// When emitting machine code from assembly, we usually have to remember a label
+// and patch it later once the address is known. This module encodes the
+// different ways to do that. Some of them correspond to relocations, while
+// others are only possible if the label is close enough.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_LC32_MCTARGETDESC_LC32FIXUPKINDS_H
+#define LLVM_LIB_TARGET_LC32_MCTARGETDESC_LC32FIXUPKINDS_H
+
+#include "llvm/MC/MCFixup.h"
+
+// I'll put this in a namespace for the LC-3.2
+namespace llvm::LC32 {
+
+enum Fixups {
+
+  // Word fixup
+  // Corresponds to R_LC_3_2_32
+  FIXUP_32 = FirstTargetFixupKind,
+
+  // Mark the end of the array
+  // Also compute the count
+  LastTargetFixupKind,
+  NumTargetFixupKinds = LastTargetFixupKind - FirstTargetFixupKind
+};
+
+} // llvm::LC32
+
+#endif // LLVM_LIB_TARGET_LC32_MCTARGETDESC_LC32FIXUPKINDS_H
