7f01bcde8b18e8b415037968c6a0902d6f1a3fb0
Ammar Ratnani <ammrat13@gmail.com>
2023-04-01 22:17:31 -0400

    Add fixup structures

---
+++ llvm/lib/Target/LC32/MCTargetDesc/LC32ELFObjectTargetWriter.h
@@ -0,0 +1,33 @@
+//===-- LC32ELFObjectWriter.h - LC-3.2 ELF Writer -------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This module provides information for translating assembly fixups to
+// relocations. The workhorse is the `getRelocType` method.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIB_TARGET_LC32_MCTARGETDESC_LC32ELFOBJECTTARGETWRITER_H
+#define LLVM_LIB_TARGET_LC32_MCTARGETDESC_LC32ELFOBJECTTARGETWRITER_H
+
+#include "llvm/MC/MCELFObjectWriter.h"
+
+namespace llvm {
+
+class LC32ELFObjectTargetWriter : public MCELFObjectTargetWriter {
+public:
+  LC32ELFObjectTargetWriter(uint8_t OSABI);
+  unsigned getRelocType(MCContext &Ctx, const MCValue &Target,
+                        const MCFixup &Fixup, bool IsPCRel) const override;
+};
+
+// Called by `LC32MCAsmBackend`
+std::unique_ptr<MCObjectTargetWriter> createLC32ELFObjectWriter(uint8_t OSABI);
+
+} // namespace llvm
+
+#endif // LLVM_LIB_TARGET_LC32_MCTARGETDESC_LC32ELFOBJECTTARGETWRITER_H
