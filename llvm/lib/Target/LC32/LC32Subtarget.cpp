426dd27c297f2c44dd1c946dacc83cfbcaa053e9
Ammar Ratnani <ammrat13@gmail.com>
2023-04-14 21:14:01 -0400

    Create subtarget

---
+++ llvm/lib/Target/LC32/LC32Subtarget.cpp
@@ -0,0 +1,21 @@
+//===-- LC32Subtarget.cpp - Define Subtarget for the LC-3.2 ----*- C++ -*--===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "LC32Subtarget.h"
+using namespace llvm;
+#define DEBUG_TYPE "LC32Subtarget"
+
+// Provides: LC32GenSubtargetInfo
+// Provides: ParseSubtargetFeatures
+#define GET_SUBTARGETINFO_TARGET_DESC
+#define GET_SUBTARGETINFO_CTOR
+#include "LC32GenSubtargetInfo.inc"
+
+LC32Subtarget::LC32Subtarget(const Triple &TT, const std::string &CPU,
+                             const std::string &FS, const TargetMachine &TM)
+    : LC32GenSubtargetInfo(TT, CPU, CPU, FS) {}
