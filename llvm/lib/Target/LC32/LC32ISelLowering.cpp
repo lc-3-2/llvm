d488e0da1ffcee4ec480c2a44ff418ad9940db35
Ammar Ratnani <ammrat13@gmail.com>
2023-04-20 23:45:47 -0400

    Do calling convention lowering

--- llvm/lib/Target/LC32/LC32ISelLowering.cpp
+++ llvm/lib/Target/LC32/LC32ISelLowering.cpp
@@ -9,21 +9,136 @@
 #include "LC32ISelLowering.h"
 #include "LC32Subtarget.h"
 #include "llvm/CodeGen/CallingConvLower.h"
+#include "llvm/CodeGen/MachineFrameInfo.h"
 using namespace llvm;
 #define DEBUG_TYPE "LC32ISelLowering"
 
 LC32TargetLowering::LC32TargetLowering(const TargetMachine &TM,
                                        const LC32Subtarget &STI)
-    : TargetLowering(TM) {}
+    : TargetLowering(TM) {
 
+  // Setup register classes
+  this->TRI = STI.getRegisterInfo();
+  this->addRegisterClass(MVT::i32, &LC32::GPRRegClass);
+  this->computeRegisterProperties(this->TRI);
+}
+
+const char *LC32TargetLowering::getTargetNodeName(unsigned Opcode) const {
+  switch (Opcode) {
+  case LC32ISD::RET:
+    return "LC32ISD::RET";
+  }
+  return nullptr;
+}
+
+// Provides: LC32CallingConv
+// Provides: LC32RetCallingConv
 #include "LC32GenCallingConv.inc"
 
+// Usually: LowerFormalArguments and LowerCCCArguments
 SDValue LC32TargetLowering::LowerFormalArguments(
     SDValue Chain, CallingConv::ID CallConv, bool isVarArg,
     const SmallVectorImpl<ISD::InputArg> &Ins, const SDLoc &dl,
     SelectionDAG &DAG, SmallVectorImpl<SDValue> &InVals) const {
+
   // We only support the one convention
   if (CallConv != CallingConv::C)
     report_fatal_error("Unsupported CallConv");
-  llvm_unreachable("UNIMPLEMENTED");
+
+  // Populate variables
+  MachineFunction &MF = DAG.getMachineFunction();
+  MachineFrameInfo &MFI = MF.getFrameInfo();
+
+  // Initialize the CCState
+  SmallVector<CCValAssign, 16> ArgLocs;
+  CCState CCInfo(CallConv, isVarArg, MF, ArgLocs, *DAG.getContext());
+  CCInfo.AnalyzeFormalArguments(Ins, LC32CallingConv);
+
+  // Create frame indicies for each of the arguments
+  // See: LanaiISelLowering.cpp
+  for (const CCValAssign &va : ArgLocs) {
+    assert(va.isMemLoc() && "LC-3.2 should have all arguments on the stack");
+
+    // Make sure object size is what we expect
+    assert(va.getValVT().getSizeInBits() == 32 &&
+           "LC-3.2 only supports words as arguments");
+
+    // Get the frame index
+    // Note the offset from the frame pointer
+    int FI = MFI.CreateFixedObject(4, 16 + va.getLocMemOffset(), true);
+    InVals.push_back(DAG.getLoad(
+        va.getLocVT(), dl, Chain, DAG.getFrameIndex(FI, MVT::i32),
+        MachinePointerInfo::getFixedStack(DAG.getMachineFunction(), FI),
+        Align(4)));
+  }
+
+  return Chain;
+}
+
+bool LC32TargetLowering::CanLowerReturn(
+    CallingConv::ID CallConv, MachineFunction &MF, bool isVarArg,
+    const SmallVectorImpl<ISD::OutputArg> &Outs, LLVMContext &Context) const {
+
+  // Initialize CCState
+  SmallVector<CCValAssign, 16> RVLocs;
+  CCState CCInfo(CallConv, isVarArg, MF, RVLocs, Context);
+
+  // Process
+  // The return value controls if sret demotion is performed.
+  return CCInfo.CheckReturn(Outs, LC32RetCallingConv);
+}
+
+SDValue
+LC32TargetLowering::LowerReturn(SDValue Chain, CallingConv::ID CallConv,
+                                bool isVarArg,
+                                const SmallVectorImpl<ISD::OutputArg> &Outs,
+                                const SmallVectorImpl<SDValue> &OutVals,
+                                const SDLoc &dl, SelectionDAG &DAG) const {
+
+  // Populate variables
+  MachineFunction &MF = DAG.getMachineFunction();
+  MachineFrameInfo &MFI = MF.getFrameInfo();
+
+  // Initialize CCState
+  SmallVector<CCValAssign, 16> RVLocs;
+  CCState CCInfo(CallConv, isVarArg, DAG.getMachineFunction(), RVLocs,
+                 *DAG.getContext());
+  CCInfo.AnalyzeReturn(Outs, LC32RetCallingConv);
+
+  // If we got here, it means we aren't demoting to sret
+  // Therefore, just lower onto the return value slot
+
+  // Operands for the return node
+  // The first is the chain operand
+  SmallVector<SDValue, 4> RetOps(1, Chain);
+
+  if (RVLocs.size() == 1) {
+    assert(Outs.size() == 1 && "More RVLocs than Outs");
+    assert(OutVals.size() == 1 && "More RVLocs than OutVals");
+
+    // Get the location
+    CCValAssign &va = RVLocs[0];
+    assert(va.isMemLoc() && "LC-3.2 can only return on the stack");
+    assert(va.getLocMemOffset() == 0 && "LC-3.2 has only one return value");
+
+    // Make sure object size is what we expect
+    assert(va.getValVT().getSizeInBits() == 32 &&
+           "LC-3.2 only supports words as returns");
+
+    // Get the frame index and store
+    int FI = MFI.CreateFixedObject(4, 12, false);
+    Chain = DAG.getStore(
+        Chain, dl, OutVals[0], DAG.getFrameIndex(FI, MVT::i32),
+        MachinePointerInfo::getFixedStack(DAG.getMachineFunction(), FI),
+        Align(4));
+    RetOps.push_back(OutVals[0]);
+
+  } else {
+    assert(RVLocs.empty() && "Should have demoted to sret");
+  }
+
+  // Update the chain for the return
+  RetOps[0] = Chain;
+
+  return DAG.getNode(LC32ISD::RET, dl, MVT::Other, RetOps);
 }
--- llvm/lib/Target/LC32/LC32ISelLowering.h
+++ llvm/lib/Target/LC32/LC32ISelLowering.h
@@ -6,8 +6,11 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This module tells other passes what operations are legal. It also provides
-// custom lowering operations as needed.
+// This module implements the bulk of lowering. While the DAGToDAG pass handles
+// instruction selection, this pass handles the calling convention and stack
+// frames.
+//
+// See: CCState
 //
 //===----------------------------------------------------------------------===//
 
@@ -18,19 +21,41 @@
 
 namespace llvm {
 class LC32Subtarget;
+class LC32RegisterInfo;
+
+// These are all the custom SelectionDAG nodes. Keep this in sync with TableGen.
+// Also update getTargetNodeName.
+namespace LC32ISD {
+enum {
+  FIRST_NUMBER = ISD::BUILTIN_OP_END,
+  RET,
+};
+} // namespace LC32ISD
 
 class LC32TargetLowering : public TargetLowering {
 public:
   explicit LC32TargetLowering(const TargetMachine &TM,
                               const LC32Subtarget &STI);
 
+  const char *getTargetNodeName(unsigned Opcode) const override;
+
 private:
+  const LC32RegisterInfo *TRI;
+
   // Calling Convention
   SDValue LowerFormalArguments(SDValue Chain, CallingConv::ID CallConv,
                                bool isVarArg,
                                const SmallVectorImpl<ISD::InputArg> &Ins,
                                const SDLoc &dl, SelectionDAG &DAG,
                                SmallVectorImpl<SDValue> &InVals) const override;
+  bool CanLowerReturn(CallingConv::ID CallConv, MachineFunction &MF,
+                      bool isVarArg,
+                      const SmallVectorImpl<ISD::OutputArg> &Outs,
+                      LLVMContext &Context) const override;
+  SDValue LowerReturn(SDValue Chain, CallingConv::ID CallConv, bool isVarArg,
+                      const SmallVectorImpl<ISD::OutputArg> &Outs,
+                      const SmallVectorImpl<SDValue> &OutVals, const SDLoc &dl,
+                      SelectionDAG &DAG) const override;
 };
 
 } // namespace llvm
