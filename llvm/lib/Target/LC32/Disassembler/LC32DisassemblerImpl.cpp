2d6a0a0fec21a8ff2a0df5fa212922b5a7b404b9
Ammar Ratnani <ammrat13@gmail.com>
2023-10-01 18:34:46 -0700

    Add comments to annotate pseudo instructions

--- llvm/lib/Target/LC32/Disassembler/LC32DisassemblerImpl.cpp
+++ llvm/lib/Target/LC32/Disassembler/LC32DisassemblerImpl.cpp
@@ -11,7 +11,9 @@
 #include "llvm/MC/MCDecoderOps.h"
 #include "llvm/MC/MCInst.h"
 #include "llvm/Support/Endian.h"
+#include "llvm/Support/Format.h"
 #include "llvm/Support/MathExtras.h"
+#include "llvm/Support/raw_ostream.h"
 using namespace llvm;
 #define DEBUG_TYPE "LC32Disassembler"
 
@@ -67,6 +69,102 @@ static DecodeStatus DecodePCOffset(MCInst &MI, uint64_t Imm, uint64_t Address,
   return DecodeStatus::Success;
 }
 
+// Functions to annotate pseudo instructions in the comment stream. These
+// functions return `true` if they match. If they don't match, further pseudo
+// instructions are tried.
+// See: LC32MCCodeEmitter::encodeInstruction
+namespace {
+
+bool annotateP_LOADCONSTH(ArrayRef<uint8_t> Bytes, raw_ostream &CStream) {
+
+  // Fetch the halfwords from the instruction
+  // We know that its in bounds since the caller checks that
+  const uint16_t lea_raw = (Bytes[1] << 8) + Bytes[0];
+  const uint16_t ldh_raw = (Bytes[3] << 8) + Bytes[2];
+  const uint16_t br_raw = (Bytes[5] << 8) + Bytes[4];
+
+  // Mask out the registers from the raw data
+  const uint16_t lea_masked = lea_raw & 0xf1ff;
+  const uint16_t ldh_masked = ldh_raw & 0xf03f;
+  const uint16_t br_masked = br_raw;
+
+  // Pull out the registers from the data
+  const unsigned lea_reg_dr = (lea_raw & 0x0e00) >> 9;
+  const unsigned ldh_reg_dr = (ldh_raw & 0x0e00) >> 9;
+  const unsigned ldh_reg_baser = (ldh_raw & 0x01c0) >> 6;
+
+  // Assert all the masked values are what we expect, and that all the registers
+  // are equal to each other
+  if (lea_masked != 0xe004 || ldh_masked != 0x6000 || br_masked != 0x0e01 ||
+      lea_reg_dr != ldh_reg_dr || ldh_reg_dr != ldh_reg_baser)
+    return false;
+
+  // Get the data being loaded
+  const int16_t data = (Bytes[7] << 8) + Bytes[6];
+  // Insert the comment and return
+  CStream << "PSEUDO.LOADCONSTH R" << lea_reg_dr << ", #" << data;
+  return true;
+}
+
+bool annotateP_LOADCONSTW(ArrayRef<uint8_t> Bytes, raw_ostream &CStream) {
+
+  // Fetch the halfwords from the instruction
+  // We know that its in bounds since the caller checks that
+  const uint16_t lea_raw = (Bytes[1] << 8) + Bytes[0];
+  const uint16_t ldw_raw = (Bytes[3] << 8) + Bytes[2];
+  const uint16_t br_raw = (Bytes[5] << 8) + Bytes[4];
+  const uint16_t trap_enc = (Bytes[7] << 8) + Bytes[6];
+
+  // Mask out the registers from the raw data
+  const uint16_t lea_masked = lea_raw & 0xf1ff;
+  const uint16_t ldw_masked = ldw_raw & 0xf03f;
+  const uint16_t br_masked = br_raw;
+  const uint16_t trap_masked = trap_enc;
+
+  // Pull out the registers from the data
+  const unsigned lea_reg_dr = (lea_raw & 0x0e00) >> 9;
+  const unsigned ldw_reg_dr = (ldw_raw & 0x0e00) >> 9;
+  const unsigned ldw_reg_baser = (ldw_raw & 0x01c0) >> 6;
+
+  // Assert all the masked values are what we expect, and that all the registers
+  // are equal to each other
+  if (lea_masked != 0xe006 || ldw_masked != 0xa000 || br_masked != 0x0e03 ||
+      trap_masked != 0xf0ff || lea_reg_dr != ldw_reg_dr ||
+      ldw_reg_dr != ldw_reg_baser)
+    return false;
+
+  // Get the data being loaded
+  const int32_t data =
+      (Bytes[11] << 24) + (Bytes[10] << 16) + (Bytes[9] << 8) + Bytes[8];
+  // Insert the comment and return
+  CStream << "PSEUDO.LOADCONSTW R" << lea_reg_dr << ", #" << data;
+  return true;
+}
+
+bool annotateP_FARJSR(ArrayRef<uint8_t> Bytes, raw_ostream &CStream) {
+
+  // Fetch the halfwords from the instruction
+  // We know that its in bounds since the caller checks that
+  const uint16_t lea_enc = (Bytes[1] << 8) + Bytes[0];
+  const uint16_t ldw_enc = (Bytes[3] << 8) + Bytes[2];
+  const uint16_t jsrr_enc = (Bytes[5] << 8) + Bytes[4];
+  const uint16_t br_enc = (Bytes[7] << 8) + Bytes[6];
+
+  // Assert all the values are what we expect
+  if (lea_enc != 0xe606 || ldw_enc != 0xa6c0 || jsrr_enc != 0x40c0 ||
+      br_enc != 0x0e02)
+    return false;
+
+  // Get the call target
+  const uint32_t target =
+      (Bytes[11] << 24) + (Bytes[10] << 16) + (Bytes[9] << 8) + Bytes[8];
+  // Insert the comment and return
+  CStream << "PSEUDO.FARJSR x" << format_hex_no_prefix(target, 8);
+  return true;
+}
+
+} // namespace
+
 // Provides: decodeInstruction
 // Requires: DecodeGPRRegisterClass
 // Requires: DecodeShiftedSignedImm
@@ -84,6 +182,21 @@ DecodeStatus LC32Disassembler::getInstruction(MCInst &MI, uint64_t &Size,
   // Can't do less than that
   if (Bytes.size() < 2)
     return DecodeStatus::Fail;
+
+  // Add comments for pseudo instructions
+  // Do it at most one time
+  // See: LC32MCCodeEmitter::encodeInstruction
+  {
+    bool keep_trying = true;
+    // Process long instructions first
+    if (keep_trying && Bytes.size() >= 12)
+      keep_trying = !annotateP_LOADCONSTW(Bytes, CStream);
+    if (keep_trying && Bytes.size() >= 12)
+      keep_trying = !annotateP_FARJSR(Bytes, CStream);
+    // Then process short instructions
+    if (keep_trying && Bytes.size() >= 8)
+      keep_trying = !annotateP_LOADCONSTH(Bytes, CStream);
+  }
 
   // Normal instruction
   // Read the next two bytes and decode with TableGen
